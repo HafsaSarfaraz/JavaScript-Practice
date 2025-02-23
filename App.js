6c2345ceab1a2c80f68ae848000c03fef49c8783
HafsaSarfaraz <154260956+HafsaSarfaraz@users.noreply.github.com>
2025-02-23 19:42:56 +0500

    Initial commit with React Native project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Word...this is my first reactnative code</Text>
       <StatusBar style="auto" />
     </View>
   );
