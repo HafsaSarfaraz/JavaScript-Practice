30acf17f5ac2e845b3b1c47956a838934909a60c
HafsaSarfaraz <154260956+HafsaSarfaraz@users.noreply.github.com>
2025-03-03 16:22:11 +0500

    Added new updates in week-4-branch where i learn how to make simple reactNative project

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './Components/Header.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Word...this is my first reactnative code</Text>
+      <Header></Header>
+      <Text>Hello Word...this is my first reactnative ...</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>  //for displaying value we use view
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    height:"100px",
+    width:"100px",
+    color:'#fff'
   },
 });
