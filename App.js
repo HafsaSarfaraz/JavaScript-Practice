b057c0c36a4b0267ed3ffbdee4d2ff1f10e344a2
HafsaSarfaraz <154260956+HafsaSarfaraz@users.noreply.github.com>
2025-03-10 10:36:30 +0500

    Added new updates in Activity1

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './Components/Header.js';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
+  const [count , setCount] = useState(0);
+  let cunt=0;
+  // Increment the value of count
+  function handledIncre(){
+    setCount(cunt+2);
+  }
+
+  // Decrement the value of count
+  function handledDecre(){
+    setCount(cunt-2);
+  }
+
+  // Reset the value of count
+  function handledReset(){
+    setCount(0);
+  }
   return (
     <View style={styles.container}>
-      <Header></Header>
-      <Text>Hello Word...this is my first reactnative ...</Text>
+      
+      <Text>Value of Counter is... {count}</Text>
+      <Button title='Increase Counter' color='red' onPress={handledIncre}></Button>
+      <Button title='decrease Counter' color='red' onPress={handledDecre}></Button>
+      <Button title='reset Counter' color='red' onPress={handledReset}></Button>
+
       <StatusBar style="auto" />
     </View>  //for displaying value we use view
   );
@@ -15,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: cunt<5? 'red':'purple',
     alignItems: 'center',
     justifyContent: 'center',
     height:"100px",
